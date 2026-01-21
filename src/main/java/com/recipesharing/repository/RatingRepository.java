95d9df57844c1e79f68fd939833585544d958121
Mohammed Zeinhom <zzzeinhom@gmail.com>
2026-01-21 05:22:55 +0200

    feat(rating): add RatingRepository
    - Add findByRecipe query method
    - Add findByUserAndRecipe for user's rating
    - Calculate average rating queries
    - Support rating statistics

---
+++ src/main/java/com/recipesharing/repository/RatingRepository.java
@@ -0,0 +1,19 @@
+package com.recipesharing.repository;
+
+import com.recipesharing.entity.Rating;
+import com.recipesharing.entity.Recipe;
+import com.recipesharing.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+    Optional<Rating> findByUserAndRecipe(User user, Recipe recipe);
+
+    boolean existsByUserAndRecipe(User user, Recipe recipe);
+
+    Page<Rating> findAllByRecipeId(Long recipeId, Pageable pageable);
+}
+
